70e2c097fccaa0f3097df403e1318dc22a9e34c9
Pradeep Kumar Siddappa Malleshappa <pradeep.ksm@flipkart.com>
2016-08-04 15:08:23 +0530

    Check

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-export default () => {
-    return "Pradeep";
+import React,{Component} from 'react';
+
+export default class SampleComponent extends Component{
+    render(){
+        return (
+            <div>
+                Sample Component
+            </div>
+        )
+    }
 }
