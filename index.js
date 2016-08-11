a8f9eb54357ae0044fa21fd0975ea62cf91c4146
Pradeep Kumar Siddappa Malleshappa <pradeep.ksm@flipkart.com>
2016-08-11 12:04:24 +0530

    master changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class SampleComponent extends Component{
         return (
             <div>
                 <Src />
-                Sample Component By Pradeep- Feature branch
+                Sample Component By Pradeep- Feature branch - master branch
             </div>
         )
     }
