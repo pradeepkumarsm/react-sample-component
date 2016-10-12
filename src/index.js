4760b4864c0a4d578369d4c1dffcd814630604e2
pradeepkumarsm <pradeepkumar.sm999@gmail.com>
2016-10-12 16:39:39 +0530

    slider again

--- src/index.js
+++ src/index.js
@@ -18,6 +18,13 @@ export default class sampleComponent extends Component{
                 master changes 23 oct
                 master changes 24 oct
                 master changes 25 oct
+               <div>
+               	check
+               </div>
+                new slider changes
+
+                slider changes again
+
             </div>
         )
     }
