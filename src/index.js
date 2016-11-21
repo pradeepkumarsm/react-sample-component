3a6b7262a3007835906d3b93805f88ece0780bdd
pradeepkumarsm <pradeepkumar.sm999@gmail.com>
2016-11-21 17:17:52 +0530

    Changes master 2

--- src/index.js
+++ src/index.js
@@ -26,6 +26,8 @@ export default class sampleComponent extends Component{
 
                 master changes 146 nov master
 
+                master changes 146 nov master
+
             </div>
         )
     }
